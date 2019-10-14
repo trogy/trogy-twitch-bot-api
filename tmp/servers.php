d89b55818c367cb091fb362f0d364543471b1728
trogy <33707105+trogy@users.noreply.github.com>
2019-10-15 00:16:14 +1300

    Servers Command Beta

--- tmp/servers.php
+++ tmp/servers.php
@@ -4,30 +4,45 @@ $api_request = file_get_contents('https://api.truckersmp.com/v2/servers');
 $search = '{"error":"false","response":';
 $step1 = str_replace($search, $replace, $api_request);
 $step2 = substr($step1, 0, -1);
- $api_response = $step2;
- echo $api_response;
- echo "<hr>";
-var_dump(json_decode($api_response, true));
- echo "<hr>";
-
-
+$api_response = $step2;
 $array = json_decode($api_response, true);
 $i = 0;
 $limit = 10;
 $count = count($array);
 $arraycount = 0;
-
 while ($i < $limit && $i < $count) {
    $data = $array[$i];
-   $arraydataname = $array[$arraycount]["name"];
+   $arraydatagame = $array[$arraycount]["game"];
    $arraydatashortname = $array[$arraycount]["shortname"];
-  echo $arraydataname;
-  echo '<br>';
-  echo $arraydatashortname;
-  echo '<br>';
-  echo '<hr>';
+   $arraydataplayers = $array[$arraycount]["players"];
+   $arraydatamaxplayers = $array[$arraycount]["maxplayers"];
+   $arraydataspeedlimiter = $array[$arraycount]["speedlimiter"];
+   $arraydataevent = $array[$arraycount]["event"];
+   echo '[' . $arraydatagame . ']';
+   echo ' ';
+  echo '[' . $arraydatashortname . ']';
+  echo ' - ';
+  echo 'Players: ' . $arraydataplayers . ' / ' . $arraydatamaxplayers;
+  if($arraydataspeedlimiter == 'true'){
+  echo ' - ';
+  echo 'Limited';
+  echo ' - ';
+  }
+  else{
+
+  }
+  if($arraydataevent == 'true'){
+  echo 'Event';
+  echo ' - ';
+  }
+  else{
+
+  }
+  echo '  |  ';
+
 
   $arraycount++;
   ++$i;
 }
+echo 'Command in beta';
   ?>
