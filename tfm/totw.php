0a1a03b454da6699e400aad99c829f3e02a58420
trogy <33707105+trogy@users.noreply.github.com>
2019-10-14 23:35:54 +1300

    kinda working tmp stats

--- tfm/totw.php
+++ tfm/totw.php
@@ -1,5 +1,5 @@
 <?php
-//A Simple API to grab the current playing track from Truckers.FM
+//Trogy Twitch Bot API (C) 2019 Marc Anderson
 $api_request = file_get_contents('https://panel.truckers.fm/api/totw');
 $api_response =  "[" . $api_request . "]";
 $array = json_decode($api_request, true);
--- tmp/servers.php
+++ tmp/servers.php
@@ -1,8 +1,33 @@
 <?php
- //A Simple API to grab the current playing track from Truckers.FM
-$api_request = file_get_contents('https://api.truckyapp.com/v2/traffic/servers');
- $api_response =  $api_request;
- $array = json_decode($api_request, true);
- $link = $array["name"];
- echo "Vote for TFM track of the week here: " . $link;
+//Trogy Twitch Bot API (C) 2019 Marc Anderson
+$api_request = file_get_contents('https://api.truckersmp.com/v2/servers');
+$search = '{"error":"false","response":';
+$step1 = str_replace($search, $replace, $api_request);
+$step2 = substr($step1, 0, -1);
+ $api_response = $step2;
+ echo $api_response;
+ echo "<hr>";
+var_dump(json_decode($api_response, true));
+ echo "<hr>";
+
+
+$array = json_decode($api_response, true);
+$i = 0;
+$limit = 10;
+$count = count($array);
+$arraycount = 0;
+
+while ($i < $limit && $i < $count) {
+   $data = $array[$i];
+   $arraydataname = $array[$arraycount]["name"];
+   $arraydatashortname = $array[$arraycount]["shortname"];
+  echo $arraydataname;
+  echo '<br>';
+  echo $arraydatashortname;
+  echo '<br>';
+  echo '<hr>';
+
+  $arraycount++;
+  ++$i;
+}
   ?>
