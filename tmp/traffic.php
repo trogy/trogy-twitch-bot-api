1d8c1255e5947493d3b4999b12c7709f478d05e8
trogy <33707105+trogy@users.noreply.github.com>
2019-10-16 01:16:08 +1300

    Kinda broken but kinda working

--- tmp/traffic.php
+++ tmp/traffic.php
@@ -1,43 +1,48 @@
 <?php
 //Trogy Twitch Bot API (C) 2019 Marc Anderson
+$inputcheck = array("ets2-sim1","ets2-sim2","ets2-arc1");
 $args = $_GET['args'];
-if (isset($_GET['args'])){
-  $argsexp = explode('-',$args);
-  echo var_dump($argsexp);
-  echo '<hr>';
-  $game = $argsexp["0"];
-  $server = $argsexp["1"];
-  $api_link = 'https://api.truckyapp.com/v2/traffic/top?game=' . $game . '&server=' . $server;
-  echo $api_link;
-  echo '<hr>';
-  $api_request = file_get_contents($api_link);
-  $search = '[{"response":';
-  $replace = '';
-  $step1 = str_replace($search, $replace, $api_request);
-  $api_response = substr($step1, 0, -1);
-  echo $api_response;
-  echo '<hr>';
-  $array = json_decode($api_request, true);
-  echo var_dump($array);
-  echo '<hr>';
-
-  $i = 0;
-  $limit = 5;
-  $count = count($array);
-  $arraycount = 0;
-  while ($i < $limit && $i < $count) {
-     $data = $array[$i];
-     $arraydataname = $array[$arraycount]["name"];
-     $arraydataseverity = $array[$arraycount]["severity"];
-     $arraydataplayers = $array[$arraycount]["players"];
-     echo " | " . $arraydataname . $arraydataseverity . $arraydataplayers;
-
-    $arraycount++;
-    ++$i;
+foreach ($inputcheck as $ic) {
+  if ($args == $ic) {
+    if (isset($_GET['args'])){
+      $argsexp = explode('-',$args);
+      $game = $argsexp["0"];
+      $server = $argsexp["1"];
+      $api_link = 'https://api.truckyapp.com/v2/traffic/top?game=' . $game . '&server=' . $server;
+      $api_request = file_get_contents($api_link);
+      $search = '{"response":';
+      $replace = '';
+      $step1 = str_replace($search, $replace, $api_request);
+      $api_response = substr($step1, 0, -1);
+      $array = json_decode($api_response, true);
+      $i = 0;
+      $limit = 5;
+      $count = count($array);
+      $arraycount = 0;
+      while ($i < $limit && $i < $count) {
+        $data = $array[$i];
+        $arraydataname = $array[$arraycount]["name"];
+        $arraydataseverity = $array[$arraycount]["severity"];
+        $arraydataplayers = $array[$arraycount]["players"];
+        if ($arraydataseverity === 'Fluid'){
+          echo $arraydataname . " Low " . $arraydataplayers . " | ";
+        }
+        else{
+          echo $arraydataname . " " . $arraydataseverity . " " . $arraydataplayers . " | ";
+        }
+        $arraycount++;
+        ++$i;
+      }
+      echo ' Command in beta';
+    }
+  else{
+    Echo "Error in input... Game Types are ets2 ats Servers are sim1 sim2 arc1 | Command in beta ATS is not supported";
+    throw new Exception('Invalid Input Type');
   }
-  echo ' Command in beta';
 }
 else{
   Echo "Error in input... Game Types are ets2 ats Servers are sim1 sim2 arc1 | Command in beta ATS is not supported";
+  throw new Exception('Invalid Input Type');
+}
 }
 ?>
