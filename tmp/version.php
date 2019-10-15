39df6a9bd5fbad5a7a40d527abcc052e2c03f4fb
trogy <33707105+trogy@users.noreply.github.com>
2019-10-15 14:04:04 +1300

    Added TMP Version

--- tmp/version.php
+++ tmp/version.php
@@ -1,3 +1,10 @@
 <?php
 //Trogy Twitch Bot API (C) 2019 Marc Anderson
+$api_request = file_get_contents('https://api.truckersmp.com/v2/version');
+$api_response =  "[" . $api_request . "]";
+$array = json_decode($api_request, true);
+$version = $array["name"];
+$atsversion = $array["supported_game_version"];
+$etsversion = $array["supported_ats_game_version"];
+echo "Current TruckersMP Version: " . $version . " Suported Game Versions: ATS - " . $atsversion . " ETS2 - " . $etsversion . " | Command In Beta";
 ?>
