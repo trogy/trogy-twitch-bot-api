ab5dcf011c3efbb1d70d9ba96a0ab729dd5a159c
trogy <33707105+trogy@users.noreply.github.com>
2019-10-18 14:38:17 +1300

    update

--- tfm/currentdj.php
+++ tfm/currentdj.php
@@ -1,10 +1,13 @@
 <?php
 //Trogy Twitch Bot API (C) 2019 Marc Anderson
 $api_request = file_get_contents('https://panel.truckers.fm/api/summary');
-$api_response =  "[" . $api_request . "]";
+$search = '{"status":"success","result":{"next":';
+$step1 = str_replace($search, $replace, $api_request);
+$step2 = substr($step1, 0, -3);
+$api_response =  "[" . $step2 . "]";
 echo $api_response;
 echo "<hr>";
-$array = json_decode($api_request, true);
+$array = json_decode($api_response, true);
 echo var_dump($array);
 echo "<hr>";
 $link = $array[""];
