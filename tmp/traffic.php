e73f3aae07b98964b2d0b5134532227567c2dc0a
trogy <33707105+trogy@users.noreply.github.com>
2019-10-18 23:23:45 +1300

    updates

--- tmp/traffic.php
+++ tmp/traffic.php
@@ -2,9 +2,8 @@
 //Trogy Twitch Bot API (C) 2019 Marc Anderson
 $inputcheck = array("ets2-sim1","ets2-sim2","ets2-arc1");
 $args = $_GET['args'];
-foreach ($inputcheck as $ic) {
-  if ($args == $ic) {
-    if (isset($_GET['args'])){
+  foreach ($inputcheck as $ic) {
+    if ($args == $ic) {
       $argsexp = explode('-',$args);
       $game = $argsexp["0"];
       $server = $argsexp["1"];
@@ -19,30 +18,31 @@ foreach ($inputcheck as $ic) {
       $limit = 5;
       $count = count($array);
       $arraycount = 0;
-      while ($i < $limit && $i < $count) {
-        $data = $array[$i];
-        $arraydataname = $array[$arraycount]["name"];
-        $arraydataseverity = $array[$arraycount]["severity"];
-        $arraydataplayers = $array[$arraycount]["players"];
-        if ($arraydataseverity === 'Fluid'){
-          echo $arraydataname . " Low " . $arraydataplayers . " | ";
-        }
-        else{
-          echo $arraydataname . " " . $arraydataseverity . " " . $arraydataplayers . " | ";
-        }
-        $arraycount++;
-        ++$i;
-      }
-      echo ' Command in beta';
-    }
-  else{
-    Echo "Error in input... Game Types are ets2 ats Servers are sim1 sim2 arc1 | Command in beta ATS is not supported";
+        while ($i < $limit && $i < $count) {
+          $data = $array[$i];
+          $arraydataname = $array[$arraycount]["name"];
+          $arraydataseverity = $array[$arraycount]["severity"];
+          $arraydataplayers = $array[$arraycount]["players"];
+            if ($arraydataseverity === 'Fluid'){
+              echo $arraydataname . " Low " . $arraydataplayers . " | ";
+            }
+            else{
+              echo $arraydataname . " " . $arraydataseverity . " " . $arraydataplayers . " | ";
+            }
+            $arraycount++;
+            ++$i;
+            }
+            echo ' Command in beta ATS is not supported';
+      
+      else{
+    Echo "Error in input... Game Types are ets2 ats Servers are sim1 sim2 arc1 FORMAT QUERY AS GAME-SERVER | Command in beta ATS is not supported";
     throw new Exception('Invalid Input Type');
-  }
+    }
 }
-else{
-  Echo "Error in input... Game Types are ets2 ats Servers are sim1 sim2 arc1 | Command in beta ATS is not supported";
-  throw new Exception('Invalid Input Type');
-}
-}
+
+
+//if (isset($_GET['args'])){
+//}
+
+
 ?>
